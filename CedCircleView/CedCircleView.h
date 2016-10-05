6d1b4f5f6552a0a20a6e725652670eda667d6c0f
gssdromen <gssdromen@gssdromens-mac-mini.local>
2016-10-05 10:25:33 +0800

    Initial Commit

---
+++ CedCircleView/CedCircleView.h
@@ -0,0 +1,19 @@
+//
+//  CedCircleView.h
+//  CedCircleView
+//
+//  Created by gssdromen on 05/10/2016.
+//  Copyright © 2016 gssdromen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CedCircleView.
+FOUNDATION_EXPORT double CedCircleViewVersionNumber;
+
+//! Project version string for CedCircleView.
+FOUNDATION_EXPORT const unsigned char CedCircleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CedCircleView/PublicHeader.h>
+
+
